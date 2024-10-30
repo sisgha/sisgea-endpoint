073d402e9422f7fb259a23a2f571ea191a3bb1d6
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2024-10-30 10:16:59 -0400

    fix(api-service): health module inside base

--- api-service/src/business-logic/resources/00-00-base/base.module.ts
+++ api-service/src/business-logic/resources/00-00-base/base.module.ts
@@ -1,11 +1,12 @@
 import { IntervaloDeTempoModule } from "@/business-logic/resources/00-00-base/intervalo-de-tempo/intervalo-de-tempo.module";
 import { Module } from "@nestjs/common";
 import { ArquivoModule } from "./arquivo/arquivo.module";
+import { HealthModule } from "./health/health.module";
 import { ImagemArquivoModule } from "./imagem-arquivo/imagem-arquivo.module";
 import { ImagemModule } from "./imagem/imagem.module";
 
 @Module({
-  imports: [ImagemModule, ArquivoModule, ImagemArquivoModule, IntervaloDeTempoModule],
+  imports: [HealthModule, ImagemModule, ArquivoModule, ImagemArquivoModule, IntervaloDeTempoModule],
   controllers: [],
   providers: [],
   exports: [],
